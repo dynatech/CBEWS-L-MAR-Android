ad3c3161a184d18f9cc69a3e181eefa0fb3fadf0
John Geliberte <gelibertejohn@gmail.com>
2019-07-25 14:56:19 +0800

    Initial commit for CBEWS-L Marirong

---
+++ android/app/src/main/java/com/cbews_l_mar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cbews_l_mar;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "CBEWS_L_MAR";
+    }
+}
