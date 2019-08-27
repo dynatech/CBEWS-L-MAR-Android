950b6c9ae108f5fa39fe5af50d32fe0a3fb5144b
John Geliberte <gelibertejohn@gmail.com>
2019-08-27 11:29:01 +0800

    + Added image viewer import

--- src/components/layout/menu/data_analysis/SurficialPlotScreen.js
+++ src/components/layout/menu/data_analysis/SurficialPlotScreen.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableHighlight, Modal } from 'react-native';
 import { ImageStyle } from '../../../../styles/image_style'
 import { ContainerStyle } from '../../../../styles/container_style'
-import { InputStyle } from '../../../../styles/input_style';
 import { LabelStyle } from '../../../../styles/label_style';
-import { ButtonStyle } from '../../../../styles/button_style';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 export default class SurficialPlotScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isGraphVisible: false,
+      surficial_plot: require('../../../../assets/test_graphs/surficial.png')
     };
   }
 
+  showGraphFunction() {
+    this.setState({ isGraphVisible: true })
+  }
+
+  hideGraphFunction() {
+    this.setState({ isGraphVisible: false })
+  }
+
   render() {
+
+    const images = [{
+      props: {
+        source: this.state.surficial_plot
+      }
+    }];
+
     return (
       <View style={ContainerStyle.content}>
         <Text style={[LabelStyle.large_label, LabelStyle.branding]}>Surficial data as of August 16, 2019 04:00 PM</Text>
-        <Image
-          style={ImageStyle.hazard_maps}
-          source={require('../../../../assets/test_graphs/surficial.png')}
-          resizeMode="contain" />
+        <TouchableHighlight onPress={() => this.showGraphFunction()}>
+            <Image
+              style={ImageStyle.graphs}
+              source={this.state.surficial_plot}
+              resizeMode="contain" />
+          </TouchableHighlight>
           <Text style={[LabelStyle.small_label, LabelStyle.brand]}>* Pinch graph to zoom in/out.</Text>
+          <Modal visible={this.state.isGraphVisible}
+            transparent={true}>
+            <ImageViewer imageUrls={images} enableSwipeDown={true} onSwipeDown={() => { this.hideGraphFunction() }} />
+          </Modal>
       </View>
     );
   }
